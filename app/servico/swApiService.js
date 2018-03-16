99a1fb7a5bb51421bf8c21d49d6321e712184363
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2018-03-16 18:35:34 -0300

    configurando timeout para api do starwars

--- app/servico/swApiService.js
+++ app/servico/swApiService.js
@@ -3,14 +3,19 @@ const fetch = require('node-fetch');
 
 class SwApiService{
 
-    constructor(config){
-        this._url = config.swApi; 
+    constructor(config,logger){
+        this._logger = logger; 
+        this._url = config.swApi.host; 
+
         this._configRequest = {
+            timeout: config.swApi.timeout,
             headers:{Accept:'application/json'}
         };  
     }
 
     obterParticipacoesDoPlanetaEmFilmes(nomeFilme){
+        this._logger.info(`Obtendo participações do planeta no filme ${nomeFilme}`); 
+
         return fetch(`${this._url}/planets?search=${nomeFilme}`,this._configRequest)
                     .then(res => res.json())
                     .then(body => body.results.reduce((quantidade,planeta) => quantidade + planeta.films.length,0))
