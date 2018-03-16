a54c33235f5e688ad62650e633a1031f52f5822c
Rodrigo Ribeiro <rodrigo.santos@m2msolutions.com.br>
2018-03-16 13:03:04 -0300

    atualizando base de teste

--- test/controlador/planetaController.js
+++ test/controlador/planetaController.js
@@ -1,42 +1,79 @@
 const request = require('supertest'); 
 const assert = require('assert'); 
 const safira = require('safira'); 
+
+const DatabaseCleaner = require('database-cleaner');
+const MongoClient = require('mongodb').MongoClient; 
+
 require('../../app/bootstrap/bootstrap'); 
+
+let app; 
+let planetaRepository; 
+let swApiService; 
+let todosOsPlanetas; 
+let databaseCleaner; 
+let config; 
+
+function salvarPlaneta(planeta){
+    return planetaRepository.adicionarPlaneta(planeta)
+}
+
+function obterPlaneta(id){
+    return planetaRepository.obterPlanetaPorId(id); 
+}
+
+function salvarPlanetas(planetas){
+    planetas.forEach(planeta => salvarPlaneta(planeta));
+}
+
+function removerPlaneta(planeta){
+    planetaRepository.removerPlaneta(planeta.id); 
+}
+
+function limparBanco(done){
+    MongoClient.connect(config.mongodb.url,(error,client) => 
+        databaseCleaner.clean(client.db('test'),() => done())
+    ); 
+}
 
 describe('Testando o controlador planetaController', () => {
 
-    var app; 
-    var planetaRepository; 
-    var swApiService; 
-
     before(() => {
         app = safira.bean('app')
+        config = safira.bean('config'); 
         planetaRepository = safira.bean('planetaRepository'); 
         swApiService = safira.bean('swApiService'); 
+        databaseCleaner = new DatabaseCleaner('mongodb'); 
+    }); 
+
+    beforeEach(done => {
+
+        limparBanco(done); 
+
+        todosOsPlanetas = [
+            {nome:"Marte",clima:"Semiárido",terreno:"deserto"}, 
+            {nome:"Plutão",clima:"Frio",terreno:"deserto"}, 
+            {nome:"Jupiter",clima:"Desértico",terreno:"deserto"}
+        ];
     }); 
 
     it('#Retornando todos os planetas',done => {
 
-        const todosOsPlanetas = [
-            {id:1,nome:"Marte",clima:"Semiárido",terreno:"deserto"}, 
-            {id:2,nome:"Plutão",clima:"Frio",terreno:"deserto"}, 
-            {id:3,nome:"Jupiter",clima:"Desértico",terreno:"deserto"}
-        ]
-
-       planetaRepository.listarPlanetas = () => Promise.resolve(todosOsPlanetas);  
+       salvarPlanetas(todosOsPlanetas); 
 
        request(app)
             .get('/v1/planetas')
             .expect('Content-Type',/json/)
             .timeout(1000) 
-            .expect(200,todosOsPlanetas) 
-            .end(done) 
+            .expect(200) 
+            .end((error,res) => {
+                assert(res.body.length === todosOsPlanetas.length)
+                done()
+            }) 
 
     }); 
 
     it("#Nenhum planeta retornado",done => {
-
-        planetaRepository.listarPlanetas = () => Promise.resolve([]); 
         
         request(app)
             .get('/v1/planetas')
@@ -48,19 +85,20 @@ describe('Testando o controlador planetaController', () => {
     it("#Retornando planeta por id",done => {
 
         const planeta = {nome:"Tatooine",clima:"arid",terreno:"desert",participacoes:5}; 
-        planetaRepository.obterPlanetaPorId = () => Promise.resolve(planeta)
+        
+        salvarPlaneta(planeta)
+            .then(planeta => {
+                request(app)
+                .get(`/v1/planetas/${planeta.id}`)
+                .expect('Content-Type',/json/) 
+                .timeout(1000)
+                .expect(200)
+                .end(done); 
 
-        request(app)
-            .get('/v1/planetas/1')
-            .expect('Content-Type',/json/) 
-            .timeout(1000)
-            .expect(200)
-            .end(done); 
+            })
     })
 
     it("#Planeta não encontrado",done => {
-
-        planetaRepository.obterPlanetaPorId = () => Promise.resolve(null)
 
         request(app)
             .get('/v1/planetas/1')
@@ -71,37 +109,30 @@ describe('Testando o controlador planetaController', () => {
 
     it("#Removendo planeta",done => {
 
-        var metodoChamado = false; 
-        planetaRepository.removerPlaneta = () => {
-            metodoChamado = true; 
-            return Promise.resolve(); 
-        }
+        const planeta = {nome:"Tatooine",clima:"arid",terreno:"desert",participacoes:5};
 
-        request(app)
-            .delete('/v1/planetas/1')
-            .timeout(1000)
-            .expect(204)
-            .end(() => {
-                assert(metodoChamado);
-                done(); 
-            }); 
-
+        salvarPlaneta(planeta)
+            .then(planeta => {
+                request(app)
+                .delete(`/v1/planetas/${planeta.id}`)
+                .timeout(1000)
+                .expect(204)
+                .end(() => {
+                    obterPlaneta(planeta.id)
+                        .then(planetaRemovido => assert(!planetaRemovido))
+                        .then(() => done())
+                }); 
+            })
     });
 
     it("#Adicionando planeta",done => {
 
         swApiService.obterParticipacoesDoPlanetaEmFilmes = () => Promise.resolve(12); 
         
-        planetaRepository.adicionarPlaneta = (planeta) => {
-            planeta.id = 1; 
-            return Promise.resolve(planeta); 
-        }
-
         const planeta = {nome:"Tatooine",clima:"arid",terreno:"desert"};
-
-        const retornoEsperado = Object.assign(planeta,{}); 
+        
+        const retornoEsperado = Object.assign({},planeta); 
         retornoEsperado.participacoes = 12; 
-        retornoEsperado.id = 1; 
 
         request(app)
             .post('/v1/planetas')
@@ -109,9 +140,15 @@ describe('Testando o controlador planetaController', () => {
             .send(planeta)
             .expect(201)
             .expect('Content-Type',/json/)
-            .expect('Location','/v1/planetas/1')
-            .expect(retornoEsperado)
-            .end(done); 
+            .expect('Location',/v1\/planetas/)
+            .end((error,res) => {
+                assert(res.body._id)
+                assert(res.body.nome === retornoEsperado.nome); 
+                assert(res.body.clima === retornoEsperado.clima);
+                assert(res.body.terreno === retornoEsperado.terreno); 
+                assert(res.body.participacoes === retornoEsperado.participacoes); 
+                done()
+            }); 
     })
 
 }); 
